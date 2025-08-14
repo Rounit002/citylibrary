d7eda8777a30bb5f0f36faffdf894161c0fe7752
Rounit Raj <rounitraj747@gmail.com>
2025-08-14 11:28:52 +0530

    login page

--- Backend/queries.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE schedules ADD COLUMN IF NOT EXISTS branch_id INTEGER;
-
-ALTER TABLE locker DROP CONSTRAINT locker_locker_number_key;
-
-ALTER TABLE locker ADD CONSTRAINT unique_locker_number_per_branch UNIQUE (locker_number, branch_id);
