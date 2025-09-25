83cbcf6910d007b05c051de4d0bc8567da52a7f2
Rounit Raj <rounitraj747@gmail.com>
2025-09-25 23:52:38 +0530

    waking up

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -103,7 +103,7 @@ const Login = () => {
           </form>
 
           <div className="mt-8 text-center text-sm text-gray-700">
-            <p>City Libarary</p>
+            <p>City Libarary, Patna City</p>
           </div>
         </div>
       </div>
